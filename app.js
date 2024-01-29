619d2cad44df0b69adbb9f16b0a5d6b904cf1a01
izdihar <izdihar@mail.com>
2024-01-29 22:59:16 +0100

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
+const Item = require("./models/Item");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+const connectInternalNetwork = "mongodb://mongodb-service:27017/my-db";
+const connectExternalNetwork = "mongodb://localhost:27018/my-db";
+
+// Connect to MongoDB
+
+mongoose
+  .connect(connectInternalNetwork , { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("error",err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World - Le bon développeur community !");
+});
+
+app.get("/items", (req, res) => {
+  Item.find()
+    .then((items) => res.send({ items }))
+    .catch((err) => res.status(500).json({ err }));
+});
+
+app.post("/items/add", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+  });
+
+  newItem
+    .save()
+    .then((item) => res.send({ message: "item saved successfully", item }))
+    .catch((err) => res.status(500).json({ err }));
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log("Server running...on port 3000"));
